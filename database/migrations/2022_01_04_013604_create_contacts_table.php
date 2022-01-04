97ee8d3b82a2260d163be2a5a198555230f79353
Eding Muhamad <masaruedo@yahoo.com>
2022-01-04 08:49:26 +0700

    create model and migration for contact

---
+++ database/migrations/2022_01_04_013604_create_contacts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone')->nullable();
+            $table->string('email');
+            $table->string('address');
+            // $table->unsignedBigInteger('company_id');
+            // $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->foreignId('company_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
