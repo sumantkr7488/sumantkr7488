a104bcc321eb0a02948f7bd3986b8333f38c4177
Eding Muhamad <masaruedo@yahoo.com>
2022-01-05 13:52:47 +0700

    Create contact controller

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\ContactController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/contacts', function () {
-    return view('contacts.index');
-})->name('contacts.index');
+Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
 
-Route::get('/contacts/create', function () {
-    return view('contacts.create');
-})->name('contacts.create');
+Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
 
-Route::get('/contacts/{id}', function ($id) {
-    $contact = App\Models\Contact::find($id);
-    return view('contacts.show', compact('contact'));
-})->name('contacts.show');
+Route::get('/contacts/{id}', [ContactController::class, 'show'])->name('contacts.show');
