89c8153b365f8dfca747d7a08fba5f2f55b04e42
Eding Muhamad <masaruedo@yahoo.com>
2022-01-06 09:24:03 +0700

    add feature to update existing contact

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@ Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.st
 Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
 
 Route::get('/contacts/{id}', [ContactController::class, 'show'])->name('contacts.show');
+
+Route::put('/contacts/{id}', [ContactController::class, 'update'])->name('contacts.update');
+
+Route::get('/contacts/{id}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
