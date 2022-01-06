df029cb92ad40749ecf90bd597e69bb10cba2e55
Eding Muhamad <masaruedo@yahoo.com>
2022-01-07 05:31:09 +0700

    add search criteria to be able to search company name

--- app/Scopes/SearchScope.php
+++ app/Scopes/SearchScope.php
@@ -13,6 +13,9 @@ class SearchScope implements Scope
             $builder->where('first_name', 'LIKE', "%{$search}%");
             $builder->orWhere('last_name', 'LIKE', "%{$search}%");
             $builder->orWhere('email', 'LIKE', "%{$search}%");
+            $builder->orWhereHas('company', function ($query) use ($search) {
+                $query->where('name', 'LIKE', "%{$search}%");
+            });
         }
     }
 }
