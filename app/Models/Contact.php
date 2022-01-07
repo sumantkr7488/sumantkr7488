d29f0b5cd622d15a09f2f3b0918d156cdd70b425
Eding Muhamad <masaruedo@yahoo.com>
2022-01-07 13:12:40 +0700

    Implement Bootable Eloquent Trait

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -2,18 +2,18 @@
 
 namespace App\Models;
 
-use App\Scopes\FilterScope;
-use App\Scopes\ContactSearchScope;
+use App\Scopes\FilterSearchScope;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Contact extends Model
 {
-    use HasFactory;
+    use HasFactory, FilterSearchScope;
 
     protected $fillable = ['first_name', 'last_name', 'email', 'phone', 'address', 'company_id'];
+    public $searchColumns = ['first_name', 'last_name', 'email'];
     public $filterColumns = ['company_id'];
-    
+
     public function company()
     {
         return $this->belongsTo(Company::class);
@@ -22,11 +22,5 @@ class Contact extends Model
     public function scopeLatestFirst($query)
     {
         return $query->orderBy('id', 'desc');
-    }
-
-    public static function booted()
-    {
-        static::addGlobalScope(new FilterScope);
-        static::addGlobalScope(new ContactSearchScope);
     }
 }
