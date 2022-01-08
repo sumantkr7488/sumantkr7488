5190d458b616474c9dccd2c6a576c469bca92e80
Eding Muhamad <masaruedo@yahoo.com>
2022-01-08 17:00:52 +0700

    Protecting contact routes with auth middleware

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $companies = Company::orderBy('name')->pluck('name', 'id')->prepend('All Companies', '');
