203e0fd2ce1a9f3c770b0969b8a504e13b626bb6
Eding Muhamad <masaruedo@yahoo.com>
2022-01-10 21:50:52 +0700

    API resource routes

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\CompanyController;
+use App\Http\Controllers\API\ContactController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// Route::apiResource('/contacts', ContactController::class);
+Route::apiResources([
+    '/contacts' => ContactController::class,
+    '/companies' => CompanyController::class
+]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
