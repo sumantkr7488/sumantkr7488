b4b7b7b2ea317a7b47b1449ea04dd4255f669fc1
Eding Muhamad <masaruedo@yahoo.com>
2022-01-10 21:15:47 +0700

    Implement resourceful routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\Settings\AccountController;
 
@@ -8,19 +9,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
+// Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
 
-Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
+// Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
 
-Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
+// Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
 
-Route::get('/contacts/{contact}', [ContactController::class, 'show'])->name('contacts.show');
+// Route::get('/contacts/{contact}', [ContactController::class, 'show'])->name('contacts.show');
 
-Route::put('/contacts/{contact}', [ContactController::class, 'update'])->name('contacts.update');
+// Route::put('/contacts/{contact}', [ContactController::class, 'update'])->name('contacts.update');
 
-Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])->name('contacts.destroy');
+// Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])->name('contacts.destroy');
 
-Route::get('/contacts/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
+// Route::get('/contacts/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
+// Route::resource('/contacts', ContactController::class);
+Route::resources([
+    '/contacts' => ContactController::class,
+    '/companies' => CompanyController::class
+]);
 
 Auth::routes(['verify' => true]);
 
