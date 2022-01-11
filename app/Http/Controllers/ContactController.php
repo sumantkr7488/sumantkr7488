5f67cc7aa0f10a8d8c40fbee52313b931a25d199
Eding Muhamad <masaruedo@yahoo.com>
2022-01-11 17:43:14 +0700

    implement eager loading in the contacts and companies queries

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -18,7 +18,7 @@ class ContactController extends Controller
     {
         $companies = Company::userCompanies();
         // \DB::enableQueryLog();
-        $contacts  = auth()->user()->contacts()->latestFirst()->paginate(10);
+        $contacts  = auth()->user()->contacts()->with('company')->latestFirst()->paginate(10);
         // dd(\DB::getQueryLog());
         return view('contacts.index', compact('contacts', 'companies'));
     }
