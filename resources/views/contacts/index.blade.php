0b4ce4e0c612183f85a8c8c3246103420c6824ee
Eding Muhamad <masaruedo@yahoo.com>
2022-01-11 07:15:48 +0700

    Display companies to the screen

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -48,10 +48,7 @@
                                 </td>
                             </tr>
                         @endforeach
-                        <form id="form-delete" method="POST" style="display: none">
-                            @csrf
-                            @method('DELETE')
-                        </form>
+                        @include('layouts._delete-form')
                     @endif
                 </tbody>
               </table> 
