625a70154fbad363b91bd42b6a74b005359e1c6f
Eding Muhamad <masaruedo@yahoo.com>
2022-01-12 19:20:27 +0700

    refactor the flash message

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -30,9 +30,8 @@
                   </tr>
                 </thead>
                 <tbody>
-                    @if ($message = session('message'))
-                        <div class="alert alert-success">{{ $message }}</div>
-                    @endif
+                    @include('layouts._message')
+                    
                     @if ($contacts->count())
                         @foreach ($contacts as $index => $contact)
                             <tr>
